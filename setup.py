c6f8d19fc2595c99abd3a7e48ce599b79d66d8b5
David <david@deadpansincerity.com>
2010-06-22 17:15:07 +0100

    restructuring and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+
+setup(name="vhosts",
+      version="0.1",
+      description="Manage Apache Virtual Hosts",
+      author="David Miller",
+      author_email="david@deadpansincerity.com",
+      url="http://www.deadpansincerity.com",
+      packages=['vhosts']
+      )
